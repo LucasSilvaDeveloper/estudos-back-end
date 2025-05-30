9d1fa0a0b0d1023471f9d78337bfd2aa48d8d72a
Felipe Lima <felipe.pereira2226@gmail.com>
2025-05-30 13:26:16 -0300

    ajuste no DTO, alteração do tipo String para o Enum TipoAtendimento.

--- src/main/java/com/elaine/projeto/atualizacao/dto/AtendimentoDTO.java
+++ src/main/java/com/elaine/projeto/atualizacao/dto/AtendimentoDTO.java
@@ -1,6 +1,7 @@
 package com.elaine.projeto.atualizacao.dto;
 
 import com.elaine.projeto.atualizacao.model.Atendimento;
+import com.elaine.projeto.atualizacao.model.TipoAtendimento;
 
 import java.time.LocalDateTime;
 
@@ -8,7 +9,7 @@ public record AtendimentoDTO(
         Long id,
         String nomeFisioterapeuta,
         String nomeAluno,
-        String tipoAtendimento,
+        TipoAtendimento tipoAtendimento,
         LocalDateTime dataAtendimento) {
 
     public Atendimento toAtendimento() {
